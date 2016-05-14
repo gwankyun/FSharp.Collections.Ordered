03cd839094f5a44aa214b3fbcc0f3a42a78dd629
gwankyun <kyunsiu@gmail.com>
2016-05-14 22:31:57 +0800

    將獨立出LazyList模塊

--- OriginalSet.fs
+++ OriginalSet.fs
@@ -5,32 +5,6 @@ open System.Collections
 open System
 open Extension
 
-type LazyList<'a>(x : 'a seq, y : 'a list) =
-    member this.Seq() = x
-    member this.List() = y
-
-module LazyList =
-    let add (value : 'a) (list : LazyList<'a>) =
-        let ls = value :: list.List()
-        let seq = ls |> Seq.ofList
-        LazyList(seq, ls)
-
-    let makeLazyListFromSeq (seq : 'a seq) =
-        let ls = List.ofSeq seq
-        LazyList(seq, ls)
-
-    let map (mapping : 'a -> 'b) (list : LazyList<'a>) =
-        let seq = list.Seq() |> Seq.map mapping
-        makeLazyListFromSeq seq
-//        let ls = List.ofSeq seq
-//        LazyList(seq, ls)
-
-    let filter (predicate : 'a -> bool) (list : LazyList<'a>) =
-        let seq = list.Seq() |> Seq.filter predicate
-        makeLazyListFromSeq seq
-//        let ls = List.ofSeq seq
-//        LazyList(seq, ls)
-
 type OriginalSet<'a when 'a : comparison>(x : 'a seq, y : Set<'a>, z : 'a list) = 
     member this.Seq() = x
     member this.Set() = y
