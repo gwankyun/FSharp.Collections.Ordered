0824e23483015aa2de57fbc08c3a0c02de7271a3
gwankyun <kyunsiu@gmail.com>
2016-05-14 22:41:14 +0800

    修改LazyList.add爲方法Cons

--- LazyList.fs
+++ LazyList.fs
@@ -5,24 +5,23 @@ open System.Collections
 open System
 open Extension
 
-type LazyList<'a>(x : 'a seq, y : 'a list) =
+type LazyList<'a>(x : 'a seq, y : 'a list) = 
     member this.Seq() = x
     member this.List() = y
-
-module LazyList =
-    let add (value : 'a) (list : LazyList<'a>) =
-        let ls = value :: list.List()
+    member this.Cons(value : 'a) = 
+        let ls = value :: this.List()
         let seq = ls |> Seq.ofList
         LazyList(seq, ls)
 
-    let makeLazyListFromSeq (seq : 'a seq) =
+module LazyList = 
+    let makeLazyListFromSeq (seq : 'a seq) = 
         let ls = List.ofSeq seq
         LazyList(seq, ls)
-
-    let map (mapping : 'a -> 'b) (list : LazyList<'a>) =
+    
+    let map (mapping : 'a -> 'b) (list : LazyList<'a>) = 
         let seq = list.Seq() |> Seq.map mapping
         makeLazyListFromSeq seq
-
-    let filter (predicate : 'a -> bool) (list : LazyList<'a>) =
+    
+    let filter (predicate : 'a -> bool) (list : LazyList<'a>) = 
         let seq = list.Seq() |> Seq.filter predicate
         makeLazyListFromSeq seq
