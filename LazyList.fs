df0f6ca170c3e0c36dc5a715c95cb47e48a056a2
gwankyun <kyunsiu@gmail.com>
2016-05-20 10:54:57 +0800

    用lazy優化LazyList

--- LazyList.fs
+++ LazyList.fs
@@ -18,45 +18,40 @@ module LazyList =
         let ls = List.ofSeq seq
         LazyList(seq, ls)
     
+    let toSeq (list : LazyList<'a>) = lazy (list.Seq() |> Seq.rev)
+    
     let map (mapping : 'a -> 'b) (list : LazyList<'a>) = 
-        let seq = list.Seq() |> Seq.map mapping
+        let seq = (toSeq list).Value |> Seq.map mapping
         makeLazyListFromSeq seq
     
     let filter (predicate : 'a -> bool) (list : LazyList<'a>) = 
-        let seq = list.Seq() |> Seq.filter predicate
+        let seq = (toSeq list).Value |> Seq.filter predicate
         makeLazyListFromSeq seq
     
     let empty<'a> = LazyList<'a>(Seq.empty, List.empty)
-
-    let toSeq (list : LazyList<'a>) =
-        lazy (list.Seq() |> Seq.rev)
-
+    
     let fold (folder : 's -> 't -> 's) (state : 's) (list : LazyList<'t>) = 
-        list
-        |> toSeq
-        |> Seq.fold folder state
-
-    let foldBack (folder : 't -> 's -> 's) (list : LazyList<'t>) (state : 's) =
-        list
-        |> fold (fun s t -> folder t s) state
-
+        let force = (list |> toSeq).Value
+        force |> Seq.fold folder state
+    
+    let foldBack (folder : 't -> 's -> 's) (list : LazyList<'t>) (state : 's) = 
+        list |> fold (fun s t -> folder t s) state
+    
     let ofArray (array : 'a []) = 
         array
         |> Seq.ofArray
         |> Seq.rev
         |> makeLazyListFromSeq
-
+    
     let ofList (elements : 'a list) = 
         elements
         |> Seq.ofList
         |> Seq.rev
         |> makeLazyListFromSeq
-
-    let ofSeq (elements : 'a seq) = 
-        elements
-        |> makeLazyListFromSeq
-
-    let partition (predicate : 'a -> bool) (list : LazyList<'a>) =
+    
+    let ofSeq (elements : 'a seq) = elements |> makeLazyListFromSeq
+    
+    let partition (predicate : 'a -> bool) (list : LazyList<'a>) = 
         let list1 = filter predicate list
-        let list2 = filter (fun x -> not(predicate x)) list
+        let list2 = filter (fun x -> not (predicate x)) list
         (list1, list2)
