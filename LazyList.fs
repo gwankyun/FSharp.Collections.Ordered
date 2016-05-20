5006e4a4ba763162dd7c667e15cda56b5db94578
gwankyun <kyunsiu@gmail.com>
2016-05-20 11:37:30 +0800

    優化LazyList

--- LazyList.fs
+++ LazyList.fs
@@ -5,30 +5,38 @@ open System.Collections
 open System
 open Extension
 
-type LazyList<'a>(x : 'a seq, y : 'a list) = 
-    member this.Seq() = x
-    member this.List() = y
+type LazyList<'a>(x : 'a list) = 
+    //    member this.Seq() = x
+    member this.List() = x
     member this.Cons(value : 'a) = 
         let ls = value :: this.List()
-        let seq = ls |> Seq.ofList
-        LazyList(seq, ls)
+        //        let seq = ls |> Seq.ofList
+        LazyList(ls)
 
 module LazyList = 
-    let makeLazyListFromSeq (seq : 'a seq) = 
-        let ls = List.ofSeq seq
-        LazyList(seq, ls)
+    //    let makeLazyListFromSeq (seq : 'a seq) = 
+    //        let ls = List.ofSeq seq
+    //        LazyList(seq, ls)
+    let ofSeq (elements : 'a seq) = LazyList(elements |> Seq.rev |> List.ofSeq)
     
-    let toSeq (list : LazyList<'a>) = lazy (list.Seq() |> Seq.rev)
+    let toSeq (list : LazyList<'a>) = 
+        lazy (list.List()
+              |> List.toSeq
+              |> Seq.rev)
     
     let map (mapping : 'a -> 'b) (list : LazyList<'a>) = 
         let seq = (toSeq list).Value |> Seq.map mapping
-        makeLazyListFromSeq seq
+        //        makeLazyListFromSeq seq
+        seq |> ofSeq
     
     let filter (predicate : 'a -> bool) (list : LazyList<'a>) = 
         let seq = (toSeq list).Value |> Seq.filter predicate
-        makeLazyListFromSeq seq
+        //        makeLazyListFromSeq seq
+        LazyList(seq
+                 |> Seq.rev
+                 |> List.ofSeq)
     
-    let empty<'a> = LazyList<'a>(Seq.empty, List.empty)
+    let empty<'a> = LazyList<'a>(List.empty)
     
     let fold (folder : 's -> 't -> 's) (state : 's) (list : LazyList<'t>) = 
         let force = (list |> toSeq).Value
@@ -41,17 +49,17 @@ module LazyList =
         array
         |> Seq.ofArray
         |> Seq.rev
-        |> makeLazyListFromSeq
+        |> ofSeq
     
     let ofList (elements : 'a list) = 
         elements
         |> Seq.ofList
         |> Seq.rev
-        |> makeLazyListFromSeq
-    
-    let ofSeq (elements : 'a seq) = elements |> makeLazyListFromSeq
+        |> ofSeq
     
     let partition (predicate : 'a -> bool) (list : LazyList<'a>) = 
-        let list1 = filter predicate list
-        let list2 = filter (fun x -> not (predicate x)) list
-        (list1, list2)
+        //        let list1 = filter predicate list
+        //        let list2 = filter (fun x -> not (predicate x)) list
+        //        (list1, list2)
+        let list1, list2 = (list |> toSeq).Value |> Seq.partition predicate
+        (ofSeq list1, ofSeq list2)
