a1098f074aac7efbf67d7cf68cfc61e5799790ed
gwankyun <kyunsiu@gmail.com>
2016-07-23 17:44:28 +0800

    增加LinkedSet.truncate

--- LinkedSet.fs
+++ LinkedSet.fs
@@ -201,3 +201,8 @@ module LinkedSet =
         table |> fold (fun (s : LinkedMap<'key, 'a>) t -> 
                      let key = projection t
                      s.Add(key, t)) (LinkedMap<'key, 'a>.Empty())
+
+    let truncate (count : int) (set : LinkedSet<'a>) =
+        set
+        |> toSeq
+        |> Seq.truncate count
